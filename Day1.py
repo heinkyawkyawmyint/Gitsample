f8a1ba04802e6d08f19f345d45e725edad770f3c
Hein Kyaw Kyaw Myint <57862286+heinkyawkyawmyint@users.noreply.github.com>
2020-04-16 10:45:10 +0630

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,5 @@
+name = "Hein Kyaw"
+age = "22"
+
+print("My name is "+name+".\n")
+print("I'm "+age+".\n")
---
+++ Variable_Lab1.py
@@ -0,0 +1,6 @@
+a = 33
+b = 22
+if b > a :
+    print("b is greater than a")
+elif a > b :
+    print("b is less than a")
